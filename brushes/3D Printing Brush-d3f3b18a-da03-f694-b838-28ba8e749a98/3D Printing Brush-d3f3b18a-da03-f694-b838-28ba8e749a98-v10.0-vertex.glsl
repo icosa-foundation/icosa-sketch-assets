6d48dc4dbdfee0363b72fbc15e7144572ed540ae
Andy Baker <andy@andybak.net>
2026-07-12 21:30:21 +0100

    Fix 3D print attribute bindings

--- brushes/3D Printing Brush-d3f3b18a-da03-f694-b838-28ba8e749a98/3D Printing Brush-d3f3b18a-da03-f694-b838-28ba8e749a98-v10.0-vertex.glsl
+++ brushes/3D Printing Brush-d3f3b18a-da03-f694-b838-28ba8e749a98/3D Printing Brush-d3f3b18a-da03-f694-b838-28ba8e749a98-v10.0-vertex.glsl
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-in vec3 position;
-in vec3 color;
+in vec3 a_position;
+in vec3 a_color;
 
 out vec4 v_color;
 out vec3 v_position;  // Camera-space position for derivatives
@@ -26,9 +26,9 @@ uniform mat4 u_SceneLight_0_matrix;
 uniform mat4 u_SceneLight_1_matrix;
 
 void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  v_position = (modelViewMatrix * vec4(position, 1.0)).xyz;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(a_position, 1.0);
+  v_position = (modelViewMatrix * vec4(a_position, 1.0)).xyz;
   v_light_dir_0 = mat3(u_SceneLight_0_matrix) * vec3(0, 0, 1);
   v_light_dir_1 = mat3(u_SceneLight_1_matrix) * vec3(0, 0, 1);
-  v_color = vec4(color, 1.0);
+  v_color = vec4(a_color, 1.0);
 }
